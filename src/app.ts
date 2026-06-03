3649ad322fe8ed131d9c491534a1274d007ec60e
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-06-03 14:33:13 +0545

    register

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import  express from 'express';
-const app  = express();
 import './database/connection'  //must necessary to import the connection file to establish the connection with database.
+import userRoute from './routes/userRoute' //importing the auth route to use it in the app.
+
+const app  = express();
+app.use(express.json()) //to parse the incoming request body in json format.
+// localhost:3000/api/auth/
+app.use('/api/auth',userRoute) //importing the auth route and using it in the app.
 
 export default app;
