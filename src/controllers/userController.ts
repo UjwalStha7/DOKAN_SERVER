654e71b2c108f1ba5c138a6d64e00a381bde3a22
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-06-03 20:36:03 +0545

    login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -30,6 +30,42 @@ class UserController{
             message : "User registered successfully"
         })
     }
+    static async login(req:Request,res:Response){
+        //accepting incoming user data(email & password))
+        const {email,password} = req.body
+        if(!email || !password){
+            res.status(400).json({
+                message : "Please provide email and password"
+            })
+            return
+        }
+
+        //checking if the email exist or not
+        const [user] = await User.findAll({ //returns an array
+            where : {
+                email : email
+            }
+        })
+
+        //email exist bhaye password verify garne
+        if(!user){
+            res.status(404).json({
+                message : "User not found"
+            })
+        }else{
+            const isEqual = bcrypt.compareSync(password, user.password) //comparing the provided password with the hashed password stored in the database.
+            if(!isEqual){
+                res.status(400).json({
+                    message : "Invalid password"
+                })
+            }else{
+                //password verify bhaye, generate token(unique identifier)
+                res.status(200).json({
+                    message : "User logged in successfully"
+                })
+            }
+        } 
+    }
 }
 
 
