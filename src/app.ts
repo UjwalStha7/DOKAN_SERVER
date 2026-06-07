2b514140620793873f7beeb02da100c76e93864b
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-06-07 17:40:00 +0545

    product controller creation

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import  express from 'express';
 import './database/connection'  //must necessary to import the connection file to establish the connection with database.
 import userRoute from './routes/userRoute' //importing the auth route to use it in the app.
 import categoryRoute from './routes/categoryRoute' //importing the category route to use it in the app.
+import productRoute from './routes/productRoute' //importing the product route to use it in the app.
 
 const app  = express();
 app.use(express.json()) //to parse the incoming request body in json format.
 // localhost:3000/api/auth/
 app.use('/api/auth',userRoute) //importing the auth route and using it in the app.
 app.use('/api/category',categoryRoute) //importing the category route and using it in the app.
+app.use('/api/product',productRoute) //importing the product route and using it in the app.
 
 export default app;
---
+++ src/controllers/productController.ts
@@ -0,0 +1,90 @@
+import {Request, Response} from 'express';
+import Product from '../database/models/productModel';
+import Category from '../database/models/categoryModel';
+
+interface ProductRequest extends Request{
+    file? : {
+        filename : string
+    }
+}
+
+class ProductController {
+    async createProduct(req:ProductRequest, res:Response):Promise<void>{
+        const {productName, productDescription, productPrice, productTotalStock, discount, categoryId} = req.body
+        const filename = req.file ? req.file.filename : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2IYhSn8Y9S9_HF3tVaYOepJBcrYcd809pBA&s"
+        if(!productName || !productDescription || !productPrice || !productTotalStock || !categoryId){
+            res.status(400).json({
+                message: "All fields are required"
+            })
+            return
+        }
+        await Product.create({
+            productName, 
+            productDescription, 
+            productPrice, 
+            productTotalStock, 
+            discount : discount || 0, 
+            categoryId,
+            productImageUrl : filename
+        })
+        res.status(200).json({
+            message: "Product created successfully"
+        })
+    }
+    async getAllProducts(req:Request, res:Response):Promise<void>{
+        const datas = await Product.findAll({
+            include : [
+                {
+                    model : Category //joining category table with product table to get category details along with product details
+                }
+            ]
+        })
+        res.status(200).json({
+            message: "Products fetched successfully",
+            data: datas
+        })
+    }
+    async getSingleProduct(req:Request, res:Response):Promise<void>{
+        const {id} = req.params
+        const datas = await Product.findAll({
+            where : {
+                id : id
+            },
+            include : [
+                {
+                    model : Category //joining category table with product table to get category details along with product details
+                }
+            ]
+        })
+        res.status(200).json({
+            message: "Products fetched successfully",
+            data: datas
+        })
+    }
+    async deleteProduct(req:Request, res:Response):Promise<void>{
+        const {id} = req.params
+        const datas = await Product.findAll({
+            where : {
+                id : id
+            }
+        })
+        if(datas.length === 0){
+            res.status(404).json({
+                message: "Product not found"
+            })
+            return
+        }else{
+            await Product.destroy({
+                where : {
+                    id : id
+                }
+            })
+            res.status(200).json({
+            message: "Product deleted successfully"
+            })
+        }  
+    }
+}
+
+export default new ProductController;
+
