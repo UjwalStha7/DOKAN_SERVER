2d0f9f0c7633d4d83324b9a735610ac05d93022c
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-06-09 12:25:19 +0545

    relation creation

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,6 +3,10 @@ import {Sequelize} from 'sequelize-typescript'; //{Sequelize} a class
 import envConfig from '../config/config';
 import Product from './models/productModel';
 import Category from './models/categoryModel';
+import Order from './models/orderModel';
+import User from './models/userModel';
+import Payment from './models/paymentModel';
+import OrderDetails from './models/orderDetails';
 
 
 const sequelize = new Sequelize(envConfig.connectionString as string,{
@@ -20,12 +24,28 @@ try{
     console.log(error);
 }
 
-sequelize.sync({force : false, alter: true}).then(()=>{
+sequelize.sync({force : false, alter: false}).then(()=>{
     console.log("local changes injected to database successfully")
 })
 
 // relationships 
 Product.belongsTo(Category, {foreignKey : 'categoryId'}) //product table ma categoryId chaiyeko hunale belongs to category
 Category.hasOne(Product, {foreignKey : 'categoryId'}) 
+
+//Order X User
+Order.belongsTo(User, {foreignKey : 'userId'})
+User.hasMany(Order, {foreignKey : 'userId'}) 
+
+//Payment X Order
+Payment.belongsTo(Order, {foreignKey : 'orderId'})
+Order.hasOne(Payment, {foreignKey : 'orderId'}) 
+
+//OrderDetails X Order
+OrderDetails.belongsTo(Order, {foreignKey : 'orderId'})
+Order.hasOne(OrderDetails, {foreignKey : 'orderId'}) 
+
+//OrderDetails X Product
+OrderDetails.belongsTo(Product, {foreignKey : 'productId'})
+Product.hasOne(OrderDetails, {foreignKey : 'productId'}) 
 
 export default sequelize;
