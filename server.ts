d3304a9cd6ad90b9cdcc742db6a5d84d7a32fadd
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-06-14 09:26:00 +0545

    updateOrderStatus feature

--- server.ts
+++ server.ts
@@ -2,9 +2,10 @@ import adminSeeder from './adminSeeder';
 import app from './src/app';
 import envConfig from './src/config/config';
 import categoryController from './src/controllers/categoryController';
-import {Server} from 'Socket.io';
+import {Server} from 'socket.io';
 import jwt from 'jsonwebtoken';
 import User from './src/database/models/userModel';
+import Order from './src/database/models/orderModel';
 
 function startServer() {
     const port = envConfig.port || 4000;
@@ -14,32 +15,57 @@ function startServer() {
         console.log(`Server is running on port ${port}`);
     });
     const io = new Server(server,{  //must provide http to know if they want to establish the connection or not. so server is send as parameter.
-        cors:{
+        cors : {
             origin : 'http://localhost:5173' //restricting access to other requests.
         }
     })
     let onlineUsers:{socketId:string, userId:string,role:string}[] = []
     let addToOnlineUsers = (socketId:string, userId:string,role:string)=>{
-        onlineUsers.filter((user)=>user.userId !== userId)
+        onlineUsers = onlineUsers.filter((user)=>user.userId !== userId)
         onlineUsers.push({socketId, userId, role})
     }
     io.on("connection", (socket) =>{
-        const {token} = socket.handshake.auth  //jwt token // same as req.header. passing data from client to server
+        const token = socket.handshake.headers.token  //jwt token // same as req.header. passing data from client to server
         if(token){
-            jwt.verify(token, envConfig.jwtSecretKey as string, async (err:any, result:any) => {
-            if (err) {
-                socket.emit("error", err)
+            console.log(token)
+            jwt.verify(token as string, envConfig.jwtSecretKey as string, async (err:any, result:any) => {
+                if (err) {
+                    socket.emit("error", err)
+                }else{
+                    const userData = await User.findByPk(result.userId)
+                    if(!userData){
+                        socket.emit("error", "No user found with that token")
+                        return
+                    } 
+                    // grab userId when user found
+                    addToOnlineUsers(socket.id, result.userId, userData.role) //connected client id.
+                    console.log(onlineUsers)
+                }
+            })
+        }else{
+            socket.emit("error", "Please provide token")
+        }
+        socket.on("updateOrderStatus", async (data)=>{
+            const {status, orderId,userId} = data
+            console.log(status, orderId)
+            //checking if user is online or not.
+            const findUser = onlineUsers.find(user=>user.userId == userId) // provide object // {socketId, userId, role}
+            await Order.update(
+                {
+                    orderStatus : status
+                },
+                {
+                    where : {
+                        id : orderId
+                    }
+                }
+            )
+            if(findUser){
+                io.to(findUser.socketId).emit("success", "Order status updated successfully")
             }else{
-                const userData = await User.findByPk(result.userId)
-                if(!userData){
-                    socket.emit("error", "No user found with that token")
-                    return
-                } 
-                // grab userId when user found
-                addToOnlineUsers(socket.id, result.userId, userData.role) //connected client id.
+                socket.emit("error", "User is not online")
             }
         })
-        }
     })
 }
 
