f6a79c85d1ae47608d69dffe9c6f323ab38eb490
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-06-21 14:17:10 +0545

    cancel order

--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -1,5 +1,5 @@
 import express, {Router} from 'express';
-import userMiddleware from '../middleware/userMiddleware';
+import userMiddleware, { Role } from '../middleware/userMiddleware';
 import orderController from '../controllers/orderController';
 import errorHandler from '../services/errorHandler';
 
@@ -7,8 +7,12 @@ import errorHandler from '../services/errorHandler';
 const router:Router = express.Router()
 
 router.route("/").post(userMiddleware.isUserLoggedIn, errorHandler(orderController.createOrder)).get(userMiddleware.isUserLoggedIn,errorHandler(orderController.fetchMyOrders))
-router.route("/:id").get(userMiddleware.isUserLoggedIn,errorHandler(orderController.fetchMyOrderDetail))
 router.route("/verify-pidx").post(userMiddleware.isUserLoggedIn, errorHandler(orderController.verifyTransaction))
+router.route("/admin/change-status/:id").patch(userMiddleware.isUserLoggedIn,userMiddleware.accessTo(Role.Admin), errorHandler(orderController.changeOrderStatus))
+router.route("/admin/delete-order/:id").post(userMiddleware.isUserLoggedIn,userMiddleware.accessTo(Role.Admin), errorHandler(orderController.deleteOrder))
+router.route("/cancel-order/:id").patch(userMiddleware.isUserLoggedIn,userMiddleware.accessTo(Role.Customer), errorHandler(orderController.cancelMyOrder))
+router.route("/:id").get(userMiddleware.isUserLoggedIn,errorHandler(orderController.fetchMyOrderDetail))
+
 
 
 
